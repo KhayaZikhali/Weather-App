65e7536c908faa973f28393ff3737c0a783e64af
KhayaZikhali <70526289+KhayaZikhali@users.noreply.github.com>
2021-02-27 22:05:20 +0100

    added weather api call
    Tried out several weather api and i settled on open weather api

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
-if (navigator.geolocation){  
-    console.log("position")    
-      navigator.geolocation.getCurrentPosition(position => {
-           console.log(position);
-           console.log("please find out")
-       })       
-    }
+window.addEventListener('load', ()=>{
+  let long;
+  let lat;
+  
+
+if (navigator.geolocation){
+  navigator.geolocation.getCurrentPosition(position => {
+    long = position.coords.longitude
+    lat = position.coords.latitude
+    console.log(long, lat)
+    const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=09b84b72c1a51001e775ea6a92b67d58`
+      fetch(api)
+           .then(response => {
+             return response.json()
+            })
+             .then(data => {
+               console.log(data);
+               const {temp , summary} = data.currently
+             })                 
+          });
+}
+} )
+
+
+
+
+
+// create and add rain aniamtion 
+//create a selector for differernt cities
