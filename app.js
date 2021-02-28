6d7810c33e9c429265c0f91b5ef0c0666d521850
KhayaZikhali <70526289+KhayaZikhali@users.noreply.github.com>
2021-02-28 01:45:44 +0100

    Added skycons master folder

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-window.addEventListener('load', ()=>{
-  // html div selectors 
+window.addEventListener('load', ()=> {
+  // html div selectors
   let long;
   let lat;
-  let temperatureDescription = document.querySelector('.temperature')
+  let temperatureDescription = document.querySelector('.temp-description')
   let temperatureDegree = document.querySelector('.temp-degree')
   let locationTimezone = document.querySelector('.location-timezone')
 
@@ -18,13 +18,25 @@ if (navigator.geolocation){
             })
             // Shows the tempperature rounded down in degreess
              .then(data => {
-               console.log(data.main);
+               console.log(data.weather[0].icon);
                const temp = data.main.temp - 273.15;
                temperatureDegree.textContent = Math.floor(temp);
+               temperatureDescription.textContent = data.weather[0].description;
+               locationTimezone.textContent = data.sys.country + " / " + data.name;
+               //set Icon function 
+               setIcon(data.weather[0].icon , document.querySelector(".icon"))
+
             })                 
         });
      }
-} )
+
+     function setIcon(icon, iconId){
+       var skycons = new Skycons({color : "white"})
+       var currentIcon = icon.replace(/-/g,"_").toUpperCase();
+       skycons.play();
+       return skycons.set(iconId , Skycons[currentIcon])
+      }
+    })
 
 
 
